3ab127ae6df1548c3a5e26bcc804d08f1c5d4351
Dakyung Seo <seoda0000@gmail.com>
2023-01-08 00:46:32 +0900

    [TIL] React Udemy : JSX 배열 다루기

--- React/02-route/src/pages/AllMeetups.js
+++ React/02-route/src/pages/AllMeetups.js
@@ -1,5 +1,35 @@
+const DUMMY_DATA = [
+  {
+    id: "m1",
+    title: "This is a first meetup",
+    image:
+      "https://www.cabq.gov/artsculture/biopark/news/10-cool-facts-about-penguins/@@images/1a36b305-412d-405e-a38b-0947ce6709ba.jpeg",
+    address: "Meetupstreet 1, 12345 Meetup City",
+    description:
+      "This is a first, amazing meetup which you definitely should not miss. Come and join us!",
+  },
+  {
+    id: "m2",
+    title: "This is a second meetup",
+    image:
+      "https://i.natgeofe.com/n/09ab5a40-53d1-4ca5-9f4c-e81011444fd1/penguins_01_3x4.jpg",
+    address: "Meetupstreet 2, 12345 Meetup City",
+    description:
+      "This is a second, amazing meetup which you definitely should not miss. Come and join us!",
+  },
+];
+
 function AllMeetupsPage() {
-  return <div>All Meetups Page</div>;
+  return (
+    <section>
+      <h1>All Meetups Page</h1>
+      <ul>
+        {DUMMY_DATA.map((meetup) => {
+          return <li key={meetup.id}>{meetup.title}</li>;
+        })}
+      </ul>
+    </section>
+  );
 }
 
 export default AllMeetupsPage;
