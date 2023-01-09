e1337f80d3440295b88c73613806ba8df68a75df
Dakyung Seo <seoda0000@gmail.com>
2023-01-09 17:16:49 +0900

    [TIL] React Udemy : Context로 데이터 관리하기

--- React/02-route/src/components/layout/MainNavigation.js
+++ React/02-route/src/components/layout/MainNavigation.js
@@ -4,7 +4,12 @@ import { Link } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
 // 모든 css가 자바스크립트 객체(classes)의 속성이 된다.
 
+import FavoritesContext from "../../store/favorite-context";
+import { useContext } from "react";
+
 function MainNavigation() {
+  const favoritesCtx = useContext(FavoritesContext);
+
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React Meetups</div>
@@ -17,7 +22,12 @@ function MainNavigation() {
             <Link to="/new-meetup">Add New Meetups</Link>
           </li>
           <li>
-            <Link to="/favorite">My Favorite</Link>
+            <Link to="/favorite">
+              My Favorite
+              <span className={classes.badge}>
+                {favoritesCtx.totalFavorites}
+              </span>
+            </Link>
           </li>
         </ul>
       </nav>
--- React/02-route/src/pages/Favorites.js
+++ React/02-route/src/pages/Favorites.js
@@ -1,5 +1,25 @@
+import { useContext } from "react";
+
+import FavoritesContext from "../store/favorite-context";
+import MeetupList from "../components/meetups/MeetupList";
+
 function FavoitesPage() {
-    return <div>FavoitesPage</div>
+  const favoritesCtx = useContext(FavoritesContext);
+
+  let content;
+
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p>You got no favorites yet.</p>;
+  } else {
+    content = <MeetupList meetups={favoritesCtx.favorites} />;
+  }
+
+  return (
+    <section>
+      <h1>My Favorites</h1>
+      {content}
+    </section>
+  );
 }
 
 export default FavoitesPage;
