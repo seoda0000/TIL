36249663eb68751d3a1c3a7401e169dc616dedfc
seoda0000 <seoda0000@gmail.com>
2023-01-10 13:47:57 +0900

    [TIL] React Udemy : React에서 Redux 사용하기

--- React/03-redux-basic/redux-demo.js
+++ React/03-redux-basic/redux-demo.js
@@ -9,6 +9,12 @@ const counterReducer = (state = { counter: 0 }, action) => {
     };
   }
 
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
   return state;
 };
 
@@ -24,3 +30,4 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber); // 리덕스에게 함수의 존재를 알려줌. 상태가 변경될 때마다 이 함수가 실행되도록 함.
 
 store.dispatch({ type: "increment" }); // 고유한 문자열로 타입을 지정해야 함.dispatch를 해서 데이터 변화.
+store.dispatch({ type: "decrement" });
