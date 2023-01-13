d17bf6736d007a6bf58a4190074091f21d33956e
seoda0000 <seoda0000@gmail.com>
2023-01-13 11:08:00 +0900

    [TIL] React Udemy: ts state 처리

--- React/07-react-typescript/src/App.tsx
+++ React/07-react-typescript/src/App.tsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 function App() {
-  const todos = [new Todo("Learn React"), new Todo("Learn TS")];
+  // const todos = [new Todo("Learn React"), new Todo("Learn TS")];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  const addTodoHandler = (todoText: string) => {};
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
 
   return (
     <div>
