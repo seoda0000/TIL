618bdd38d64e15e6beefbed04a35f329861032ee
seoda0000 <seoda0000@gmail.com>
2023-01-17 10:44:07 +0900

    [TIL] React Udemy: redux toolkit 사용하기 (createSlice)

--- React/04-redux/src/store/index.js
+++ React/04-redux/src/store/index.js
@@ -1,36 +1,26 @@
-import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
-const counterReducer = (state = initialState, action) => {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter,
-    };
-  }
-  if (action.type === "increase") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter,
-    };
-  }
 
-  if (action.type === "toggle") {
-    return {
-      counter: state.counter,
-      showCounter: !state.showCounter,
-    };
-  }
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    }, // redux toolkit: 자동으로 새로운 객체 생성 후 오버라이드
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
-  return state;
-};
-
-const store = createStore(counterReducer);
+const store = configureStore({ reducer: { counter: counterSlice.reducer } }); // 여러 리듀서 통합
 
 export default store;
