7c7adbc14db979a100e6d977abbc18dae60f4e9d
seoda0000 <seoda0000@gmail.com>
2023-01-19 10:26:39 +0900

    [TIL] React Udemy: Redux toolkit 복습

--- React/05-redux-expert/src/components/Cart/CartItem.js
+++ React/05-redux-expert/src/components/Cart/CartItem.js
@@ -1,14 +1,32 @@
-import classes from './CartItem.module.css';
+import classes from "./CartItem.module.css";
+import { cartActions } from "../../store/cart-slice";
+import { useDispatch } from "react-redux";
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const dispatch = useDispatch();
+
+  const { title, quantity, total, price, id } = props.item;
+
+  const removeItemHandler = () => {
+    dispatch(cartActions.removeItemFromCart(id));
+  };
+
+  const addItemHandler = () => {
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
+  };
 
   return (
     <li className={classes.item}>
       <header>
         <h3>{title}</h3>
         <div className={classes.price}>
-          ${total.toFixed(2)}{' '}
+          ${total.toFixed(2)}{" "}
           <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
         </div>
       </header>
@@ -17,8 +35,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeItemHandler}>-</button>
+          <button onClick={addItemHandler}>+</button>
         </div>
       </div>
     </li>
