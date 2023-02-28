a000ddd8a9099705129ce1a524778f209debcda6
Dakyung Seo <seoda0000@gmail.com>
2023-02-28 09:14:21 +0900

    [TIL] APS: 코테 스터디

--- AlgorithmProblemSolving/04_백준/Gold/7662_이중_우선순위_큐.py
+++ AlgorithmProblemSolving/04_백준/Gold/7662_이중_우선순위_큐.py
@@ -18,23 +18,34 @@ for _ in range(N):
             heapq.heappush(minheap, (num, i, num))
             heapq.heappush(maxheap, (-num, i, num))
         elif num == 1:
-            numA, id, numB = heapq.heappop(maxheap)
-            while id in delset:
-                numA, id, numB = heapq.heappop(maxheap)
-            delset.add(id)
+            if maxheap:
+                id = heapq.heappop(maxheap)[1]
+                while maxheap and (id in delset):
+                    id = heapq.heappop(maxheap)[1]
+                delset.add(id)
         else:
-            numA, id, numB = heapq.heappop(minheap)
-            while id in delset:
-                numA, id, numB = heapq.heappop(minheap)
-            delset.add(id)
+            if minheap:
+                id = heapq.heappop(minheap)[1]
+                while minheap and (id in delset):
+                    id = heapq.heappop(minheap)[1]
+                delset.add(id)
 
-    numA, id, nummax = heapq.heappop(maxheap)
-    while id in delset:
-        numA, id, nummax = heapq.heappop(maxheap)
+    flag = 0
+    if maxheap:
+        _, id, nummax = heapq.heappop(maxheap)
+    else:
+        flag = 1
+    while (flag == 0) and (id in delset):
+        if maxheap:
+            _, id, nummax = heapq.heappop(maxheap)
+        else:
+            flag = 1
 
-    numA, id, nummin = heapq.heappop(minheap)
-    while id in delset:
-        numA, id, nummin = heapq.heappop(minheap)
-
-
-
+    if flag:
+        print("EMPTY")
+    else:
+        if minheap:
+            _, id, nummin = heapq.heappop(minheap)
+        while minheap and (id in delset):
+            _, id, nummin = heapq.heappop(minheap)
+        print(nummax, nummin)
