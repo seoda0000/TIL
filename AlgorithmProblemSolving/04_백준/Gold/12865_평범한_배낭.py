3fc6cce8c42b86cbe8bd38e76ee30bb1ad7bbadf
Dakyung Seo <seoda0000@gmail.com>
2023-03-02 11:52:12 +0900

    [TIL] APS: 230302 알고 스터디

---
+++ AlgorithmProblemSolving/04_백준/Gold/12865_평범한_배낭.py
@@ -0,0 +1,38 @@
+"""
+백준 골드5 12865
+평범한 배낭
+https://www.acmicpc.net/problem/12865
+
+이 문제는 아주 평범한 배낭에 관한 문제이다.
+
+한 달 후면 국가의 부름을 받게 되는 준서는 여행을 가려고 한다. 세상과의 단절을 슬퍼하며 최대한 즐기기 위한 여행이기 때문에, 가지고 다닐 배낭 또한 최대한 가치 있게 싸려고 한다.
+
+준서가 여행에 필요하다고 생각하는 N개의 물건이 있다. 각 물건은 무게 W와 가치 V를 가지는데, 해당 물건을 배낭에 넣어서 가면 준서가 V만큼 즐길 수 있다. 아직 행군을 해본 적이 없는 준서는 최대 K만큼의 무게만을 넣을 수 있는 배낭만 들고 다닐 수 있다. 준서가 최대한 즐거운 여행을 하기 위해 배낭에 넣을 수 있는 물건들의 가치의 최댓값을 알려주자.
+"""
+
+import sys
+input = sys.stdin.readline
+
+
+N, K = map(int, input().split())
+lst = [list(map(int, input().split())) for _ in range(N)]
+
+arr = [[0] * (K+1) for _ in range(N)]
+if lst[0][0] <= K:
+    for i in range(lst[0][0], K+1):
+        arr[0][i] = lst[0][1]
+
+for n in range(1, N):
+    for k in range(K+1):
+        W = lst[n][0]
+        V = lst[n][1]
+
+        if k < W:
+            arr[n][k] = arr[n-1][k]
+
+        else:
+            arr[n][k] = max(arr[n-1][k], arr[n-1][k-W] + V)
+
+print(arr[N-1][K])
+
+
