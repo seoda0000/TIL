2393db95971915b0dbbd0842e7ab72f27613623c
Dakyung Seo <seoda0000@gmail.com>
2023-05-07 23:51:51 +0900

    Android: logging interceptor

--- AndroidStudio/retrofit/RetrofitClient.kt
+++ AndroidStudio/retrofit/RetrofitClient.kt
@@ -12,6 +12,15 @@ object RetrofitClient {
     fun getClient(): Retrofit? {
 
         val client = OkHttpClient.Builder()
+         val loggingInterceptor = HttpLoggingInterceptor(object : HttpLoggingInterceptor.Logger {
+            override fun log(message: String) {
+                Log.d("RetrofitClientLog", message)
+            }
+        })
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY // 로그 레벨 설정
+        client.addInterceptor(loggingInterceptor)
+
+
 
         var baseParameterInterceptor: Interceptor = (object : Interceptor {
             override fun intercept(chain: Interceptor.Chain): Response {
