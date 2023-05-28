014fbf1d4fa11172bd8ba1e6313e0386c4317a98
Dakyung Seo <seoda0000@gmail.com>
2023-05-28 19:43:35 +0900

    Pandas: 조건 행 찾기

--- Python/pandas.py
+++ Python/pandas.py
@@ -3,3 +3,6 @@ RAW_DATA = pd.read_excel("/fileName.xlsx",
                          sheet_name="sheetName", header=[0,1,2])
 # 헤더 하나 제거
 RAW_DATA.columns.droplevel(0)
+
+# 조건에 맞는 행 찾기
+RAW_DATA.loc[RAW_DATA['FDAgroup1'] == 2]
