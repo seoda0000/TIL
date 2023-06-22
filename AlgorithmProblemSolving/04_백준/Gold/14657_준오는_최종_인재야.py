f5779752a4b41c1bf36d696c0f1fbd5bdb6864e3
Dakyung Seo <seoda0000@gmail.com>
2023-06-22 23:51:13 +0900

    APS: 알고 스터디

--- AlgorithmProblemSolving/04_백준/Gold/14657_준오는_최종_인재야.py
+++ AlgorithmProblemSolving/04_백준/Gold/14657_준오는_최종_인재야.py
@@ -1,42 +1,39 @@
-from math import ceil
 import sys
 sys.setrecursionlimit(10**9)
 input = sys.stdin.readline
 
 
-def dfs(now, cnt):
-    global dist, endpoint, min_t
+def dfs(i, cnt):
+    global dist, end, mnT
 
     if dist < cnt:
         dist = cnt
-        endpoint = now
-        min_t = visited[now]
-    elif dist == cnt and visited[now] < min_t:
-        endpoint = now
-        min_t = visited[now]
+        end = i
+        mnT = visited[i]
+    elif dist == cnt and visited[i] < mnT:
+        end = i
+        mnT = visited[i]
 
-    for nxt, time in graph[now]:
-        if visited[nxt] == -1:
-            visited[nxt] = visited[now] + time
-            dfs(nxt, cnt + 1)
+    for g in range(N+1):
+        if graph[i][g] != 0 and visited[g] == -1:
+            visited[g] = visited[i] + graph[i][g]
+            dfs(g, cnt + 1)
 
 
-N, T = map(int, input().split()) # 문제 수, 하루 풀이 시간 한계
-graph = [[] for _ in range(N+1)]
+N, T = map(int, input().split())
+graph = [[0]*(N+1) for _ in range(N+1)]
 for _ in range(N - 1):
     A, B, C = map(int, input().split())
-
-    graph[A].append((B, C))
-    graph[B].append((A, C))
+    graph[A][B] = graph[B][A] = C
 
 
-dist, endpoint, min_t = 0, 0, int(1e9)
+dist, end, mnT = 0, 1, 100000**50000 + 1
 visited = [-1] * (N+1)
 visited[0] = 0
 dfs(1, 1)
 
 visited = [-1] * (N+1)
-dist, min_t = 0, int(1e9)
-visited[endpoint] = 0
-dfs(endpoint, 1)
-print(ceil(min_t / T))
+dist, mnT = 0, 100000**50000 + 1
+visited[end] = 0
+dfs(end, 1)
+print(mnT // T + int(mnT%T!=0))
