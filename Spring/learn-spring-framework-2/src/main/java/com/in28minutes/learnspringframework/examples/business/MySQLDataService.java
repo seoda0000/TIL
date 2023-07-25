379f2352d2745831c4a41f1ece2b51f2c3da6288
Dakyung Seo <seoda0000@gmail.com>
2023-07-26 08:58:11 +0900

    Spring: module

--- Spring/learn-spring-framework-2/src/main/java/com/in28minutes/learnspringframework/examples/business/MySQLDataService.java
+++ Spring/learn-spring-framework-2/src/main/java/com/in28minutes/learnspringframework/examples/business/MySQLDataService.java
@@ -1,8 +1,10 @@
 package com.in28minutes.learnspringframework.examples.business;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+//@Component
+@Repository
 public class MySQLDataService implements DataService {
 	
 	@Override
