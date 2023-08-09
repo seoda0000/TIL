83c29da56a1333e9a1b53c609e955b40183e86b2
Dakyung Seo <seoda0000@gmail.com>
2023-08-09 20:31:17 +0900

    Spring: Integration Test

--- Spring/02-kmh/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ Spring/02-kmh/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,22 +1,26 @@
 package hello.hellospring;
-
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
+    private final DataSource dataSource;
 
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
-
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
-
-}
+// return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+    }}
