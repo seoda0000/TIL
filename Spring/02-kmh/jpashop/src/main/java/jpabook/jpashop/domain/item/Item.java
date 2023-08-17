0c334d9dafd304b8f912a2e08c9aabe185f9ad04
Dakyung Seo <seoda0000@gmail.com>
2023-08-17 11:03:40 +0900

    Spring: item entity logic

--- Spring/02-kmh/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ Spring/02-kmh/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -11,6 +11,7 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.ManyToMany;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -31,5 +32,22 @@ public class Item {
 
     @ManyToMany(mappedBy = "items") // 실무에서는 ManyToMany 쓰지 않는 방향으로
     private List<Category> categories = new ArrayList<>();
+
+//    ==비즈니스 로직==
+
+//    stock 증가
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+//    stock 감소
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+
+        this.stockQuantity = restStock;
+    }
 
 }
