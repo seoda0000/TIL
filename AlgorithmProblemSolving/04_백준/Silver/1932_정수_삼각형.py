b122d1ac802b8ca1c4376f62b87a067efccc5858
seoda0000 <seoda0000@gmail.com>
2024-01-18 15:10:24 +0900

    [APS] 백준 1932

---
+++ AlgorithmProblemSolving/04_백준/Silver/1932_정수_삼각형.py
@@ -0,0 +1,29 @@
+"""
+https://www.acmicpc.net/problem/1932
+백준 실버1 정수 삼각형
+        7
+      3   8
+    8   1   0
+  2   7   4   4
+4   5   2   6   5
+위 그림은 크기가 5인 정수 삼각형의 한 모습이다.
+
+맨 위층 7부터 시작해서 아래에 있는 수 중 하나를 선택하여 아래층으로 내려올 때, 이제까지 선택된 수의 합이 최대가 되는 경로를 구하는 프로그램을 작성하라. 아래층에 있는 수는 현재 층에서 선택된 수의 대각선 왼쪽 또는 대각선 오른쪽에 있는 것 중에서만 선택할 수 있다.
+
+삼각형의 크기는 1 이상 500 이하이다. 삼각형을 이루고 있는 각 수는 모두 정수이며, 범위는 0 이상 9999 이하이다.
+"""
+n = int(input())
+triangle = []
+for _ in range(n):
+    triangle.append(list(map(int, input().split())))
+
+for i in range(1, n):
+    for j in range(i+1):
+        if j == 0:
+            triangle[i][j] += triangle[i-1][j]
+        elif j == i:
+            triangle[i][j] += triangle[i-1][j-1]
+        else:
+            triangle[i][j] += max(triangle[i-1][j], triangle[i-1][j-1])
+
+print(max(triangle[n-1]))
