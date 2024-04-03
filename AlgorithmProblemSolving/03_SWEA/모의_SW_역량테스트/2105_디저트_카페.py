220c7ad8b3b714cacee0fb0e27a592627389ba0e
seoda0000 <seoda0000@gmail.com>
2024-04-03 09:02:36 +0900

    APS: SWEA

--- AlgorithmProblemSolving/03_SWEA/모의_SW_역량테스트/2105_디저트_카페.py
+++ AlgorithmProblemSolving/03_SWEA/모의_SW_역량테스트/2105_디저트_카페.py
@@ -34,32 +34,111 @@ https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5
 만약, 디저트를 먹을 수 없는 경우 -1을 출력한다.
 '''
 
-
 import sys
+
 sys.stdin = open('s.txt', 'r')
 
 dr = [(1, 1), (1, -1), (-1, -1), (-1, 1)]
+
+
 def f(si, sj, ci, cj, lst, d):
     global ans
-    if ci == si+1 and cj == sj-1 and len(lst) > 1 and d == 3:
+    if ci == si + 1 and cj == sj - 1 and len(lst) > 1 and d == 3:
         if ans < len(lst):
             ans = len(lst)
         return
 
     a, b = dr[d]
-    if 0<=ci+a<N and 0<=cj+b<N and arr[ci+a][cj+b] not in lst:
-        f(si, sj, ci + a, cj + b, lst+[arr[ci+a][cj+b]], (d + 1) % 4)
+    if 0 <= ci + a < N and 0 <= cj + b < N and arr[ci + a][cj + b] not in lst:
+        f(si, sj, ci + a, cj + b, lst + [arr[ci + a][cj + b]], (d + 1) % 4)
         if cj + b != N - 1 and ci + a != N - 1:
-            f(si, sj, ci+a, cj+b, lst+[arr[ci+a][cj+b]], d)
+            f(si, sj, ci + a, cj + b, lst + [arr[ci + a][cj + b]], d)
 
 
 T = int(input())
-for tc in range(1, T+1):
+for tc in range(1, T + 1):
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)]
     ans = -1
-    for i in range(N-2):
-        for j in range(1, N-1):
+    for i in range(N - 2):
+        for j in range(1, N - 1):
             f(i, j, i, j, [arr[i][j]], 0)
 
     print(f'#{tc}', ans)
+
+"""
+1년 후 풀이
+"""
+
+"""
+9:14 시작
+9:22 구상 완료
+9:43 구현 완료
+
+직사각형이 되려면 마주보는 면의 길이가 같아야 한다는 점,
+그리고 어느 방향으로 돌아도 같은 직사각형인 점을 이용하여 각 변의 경우에 따라 분기하여 dfs로 풀었다.
+
+다음 단계로 넘겨줄 때 모든 변이 아닌 직전의 꼭짓점만을 넘겨줘서 디버깅을 했다.
+
+1년 전에는 직사각형이 만들어지든 말든 일단 현재 방향, 다음 방향으로 모두 보내버리고 마지막에 돌아왔는지만을 체크했다.
+조건이 없어서 코드가 훨씬 짧다.
+참고할만한 방법인 것 같다
+"""
+
+import sys
+
+sys.stdin = open("input.txt", "r")
+
+
+def OOB(i, j):
+    return not (0 <= i < N and 0 <= j < N)
+
+
+def draw(si, sj, num_lst, d, line_lst):  # 시작 좌표, 여태까집 방문한 디저트 가게의 종류 리스트, 현재 그릴 변의 방향, 변의 길이 리스트
+    global ans
+    ci, cj = si, sj
+
+    if d == 0 or d == 1:  # 첫 두 변
+        k = 0
+        nums = []
+        while True:
+            k += 1
+            ni, nj = ci + di[d], cj + dj[d]
+            if OOB(ni, nj): return
+            if arr[ni][nj] in num_lst or arr[ni][nj] in nums: return
+            nums.append(arr[ni][nj])
+            draw(ni, nj, num_lst + nums, d + 1, line_lst + [k])
+            ci, cj = ni, nj
+    elif d == 2 or d == 3:  # 다음 두 변
+        k = line_lst[d - 2]
+        if d == 3:
+            k -= 1
+        nums = []
+        for _ in range(k):
+            ni, nj = ci + di[d], cj + dj[d]
+            if OOB(ni, nj): return
+            if arr[ni][nj] in num_lst or arr[ni][nj] in nums: return
+            nums.append(arr[ni][nj])
+            ci, cj = ni, nj
+        draw(ci, cj, num_lst + nums, d + 1, line_lst)
+    else:  # 다 그림
+
+        ans = max(ans, len(num_lst))
+        return
+
+    return
+
+
+di = [1, 1, -1, -1]
+dj = [-1, 1, 1, -1]
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    ans = -1
+
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    for si in range(N - 2):
+        for sj in range(1, N - 1):
+            draw(si, sj, [arr[si][sj]], 0, [])
+    print(f'#{test_case} {ans}')
